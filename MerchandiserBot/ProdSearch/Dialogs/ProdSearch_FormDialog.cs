fd9ca3012f64d6e772c4bdb68aa1d60a4a9a4aed
sker1307 <38415009+sker1307@users.noreply.github.com>
2018-04-24 14:24:03 +0800

    prodsearch update

---
+++ MerchandiserBot/ProdSearch/Dialogs/ProdSearch_FormDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace MerchandiserBot.ProdSearch.Dialogs
+{
+    [Serializable]
+    public class ProdSearch_FormDialog : IDialog<IMessageActivity>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result;
+
+            // TODO: Put logic for handling user message here
+
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task ShowOptionsAsync(IDialogContext context)
+        {
+            
+        }
+    }
+}
---
+++ MerchandiserBot/ProdSearch/Dialogs/ProdSearch_KeywordDialog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace MerchandiserBot.ProdSearch.Dialogs
+{
+    [Serializable]
+    public class ProdSearch_KeywordDialog : IDialog<IMessageActivity>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("請輸入關鍵字：");
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var activity = await result;
+            if (result!=null)
+            {
+                context.Done(context);
+            }
+            // TODO: Put logic for handling user message here
+
+            //context.Wait(MessageReceivedAsync);
+        }
+    }
+}
